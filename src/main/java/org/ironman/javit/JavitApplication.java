e783e1f17ba2bcf69251fd8dfd297c4e5a0a7ab8
pana <yoophochana.p@gmail.com>
2020-02-02 13:47:51 +0700

    javit

---
+++ src/main/java/org/ironman/javit/JavitApplication.java
@@ -0,0 +1,13 @@
+package org.ironman.javit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ironman/javit/JavitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ironman.javit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
