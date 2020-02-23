108d91169c2c53918c483dc3d6522198c851ace1
Phanomphon Yotchon <phanomphon.yotchon@gmail.com>
2020-02-23 17:16:55 +0700

    clean up

--- src/main/java/org/ironman/javit/repository/ItemRepository.java
+++ src/main/java/org/ironman/javit/repository/ItemRepository.java
@@ -1,14 +1,7 @@
 package org.ironman.javit.repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map.Entry;
-
 import org.ironman.javit.entity.Item;
-import org.springframework.beans.BeanUtils;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 /**
